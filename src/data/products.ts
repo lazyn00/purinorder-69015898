4805fd9cc6fb612e356e6bd347267e36d33b8837
lazyn00 <nhuy7890@gmail.com>
2025-11-12 15:48:41 +0700

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -83,42 +83,41 @@ export const productsData = [
     master: "Boky Buyer (xhs)"
   },
   {
-    id: 4,
-    name: "Kẹp giấy",
-    price: 48000,
-    priceDisplay: "48,000đ",
-    description: [
-    ], // <- thêm dấu phẩy ở đây
-    images: [
-      "https://i.imgur.com/rNW9oZf.jpeg",
-      "https://i.imgur.com/VfqAsU1.jpeg",
-      "https://i.imgur.com/VYjpBAp.jpeg",
-      "https://i.imgur.com/1wwHmvm.jpeg",
-      "https://i.imgur.com/3Zav3gC.jpeg",
-      "https://i.imgur.com/ObfkAE5.jpeg",
-      "https://i.imgur.com/0QitLYe.jpeg"
-    ],
-    category: "Merch",
-    artist: "CORTIS",
-    variants: {
-  Type: ["Viền trong", "Viền màu"],
-  Name: ["James", "Juhoon", "Martin", "Seunghyeon", "Keonho", "Full Set 5 members"]
-},
-variantImageMap: {
-  "Viền trong-Full Set 5 members": 0,
-  "Viền màu-Full Set 5 members": 1,
-  "Viền trong-James": 2,
-  "Viền màu-James": 2,
-  "Viền trong-Juhoon": 3,
-  "Viền màu-Juhoon": 3,
-  "Viền trong-Martin": 4,
-  "Viền màu-Martin": 4,
-  "Viền trong-Seunghyeon": 5,
-  "Viền màu-Seunghyeon": 5,
-  "Viền trong-Keonho": 6,
-  "Viền màu-Keonho": 6
-},
-    feesIncluded: false,
-    master: "Boky Buyer (xhs)"
+  id: 4,
+  name: "Kẹp giấy",
+  price: 48000,
+  priceDisplay: "48,000đ",
+  description: [],
+  images: [
+    "https://i.imgur.com/rNW9oZf.jpeg",
+    "https://i.imgur.com/VfqAsU1.jpeg",
+    "https://i.imgur.com/VYjpBAp.jpeg",
+    "https://i.imgur.com/1wwHmvm.jpeg",
+    "https://i.imgur.com/3Zav3gC.jpeg",
+    "https://i.imgur.com/ObfkAE5.jpeg",
+    "https://i.imgur.com/0QitLYe.jpeg"
+  ],
+  category: "Merch",
+  artist: "CORTIS",
+  variants: {
+    Type: ["Viền trong", "Viền màu"],
+    Name: ["James","Juhoon","Martin","Seunghyeon","Keonho","Full Set 5 members"]
   },
+  variantImageMap: {
+    "Viền trong-Full Set 5 members": 0,
+    "Viền màu-Full Set 5 members": 1,
+    "Viền trong-James": 2,
+    "Viền màu-James": 2,
+    "Viền trong-Juhoon": 3,
+    "Viền màu-Juhoon": 3,
+    "Viền trong-Martin": 4,
+    "Viền màu-Martin": 4,
+    "Viền trong-Seunghyeon": 5,
+    "Viền màu-Seunghyeon": 5,
+    "Viền trong-Keonho": 6,
+    "Viền màu-Keonho": 6
+  },
+  feesIncluded: false,
+  master: "Boky Buyer (xhs)"
+},
 ];
