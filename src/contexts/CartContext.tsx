948868af6fe2aa6a408e2113ab72168ca7e5fee4
lazyn00 <nhuy7890@gmail.com>
2025-11-15 11:12:29 +0700

    Update CartContext.tsx

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 
 // === DÁN URL APPS SCRIPT (ĐỌC SẢN PHẨM) CỦA BẠN VÀO ĐÂY ===
-const GAS_PRODUCTS_URL = "https://script.google.com/macros/s/AKfycbwGuJKbuqEwF6C6wvXVygOS-uFQN7UU_9Zv7EgQ7Ved-jLRoHPd9HWzoNz034oh9THNRw/exec";
+const GAS_PRODUCTS_URL = "https://script.google.com/macros/s/AKfycbwBR9KUNgR8M5VLMGOP8YUUvwYDoTcE9AiSIxHBAhuXuXvwnEKQTapI-nRCLGYEoso5tQ/exec";
 // ===
 
 // Định nghĩa kiểu Product mới (từ Google Sheet)
