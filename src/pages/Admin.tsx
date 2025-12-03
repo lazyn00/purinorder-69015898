7b708bc1e399e0feae56989e78e506d543df68dc
lazyn00 <nhuy7890@gmail.com>
2025-12-03 15:30:44 +0700

    Update Admin.tsx

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1042,7 +1042,7 @@ ${generateEmailContent(order)}
                     <Package className="h-4 w-4 text-muted-foreground" />
                   </CardHeader>
                   <CardContent>
-                    <div className="text-2xl font-bold">{productStats.length}</div>
+                    <div className="text-2xl font-bold">{categoryStats.length}</div>
                   </CardContent>
                 </Card>
 
