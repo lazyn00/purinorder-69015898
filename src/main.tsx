6ced551d5e58efb708ce4faced598925f2c132cd
lazyn00 <nhuy7890@gmail.com>
2026-05-15 22:35:58 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+// Thêm 3 dòng này lên ĐẦU TIÊN của file src/main.tsx
+if (typeof (window as any).global === 'undefined') {
+  (window as any).global = window;
+}
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
